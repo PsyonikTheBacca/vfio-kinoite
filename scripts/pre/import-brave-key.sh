86561a77e7c0e4d3e639a60a4f70314354045444
Psyonik <40005029+PsyonikTheBacca@users.noreply.github.com>
2023-05-27 04:37:31 +0000

    Delete import-brave-key.sh

--- scripts/pre/import-brave-key.sh
+++
@@ -1,9 +0,0 @@
-set -oue pipefail
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-rpm-ostree install rpmrebuild
-mkdir brave && cd brave
-mkdir output
-wget https://brave-browser-rpm-release.s3.brave.com/x86_64/brave-keyring-1.11-1.noarch.rpm -O brave-keyring.rpm
-export RPM_BUILD_ROOT=$(pwd)/output
-sudo rpmrebuild --change-spec-post="sed 's/service atd start//;s/echo \"sh \/etc\/cron.daily\/brave-key-updater\" | at now + 2 minute > \/dev\/null 2>&1//'" --define "_rpmdir $(pwd)" -p ./brave-keyring.rpm
-sudo rpm-ostree install ./noarch/brave-keyring-1.11-1.noarch.rpm --uninstall=rpmrebuild
